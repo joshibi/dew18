c6377c713a030ccee602ed22b21833332beb7ac4
thirozumi <thirozumi@gmail.com>
2018-07-02 21:45:38 +0900

    added kawakami-san

---
+++ 173044/assets/scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let getStatus = function(e) {
+  let status = navigator.onLine ? 'online' : 'offline';
+  let b = document.getElementsByTagName('body')[0];
+  b.className = '';
+  b.classList.add(status);
+}
+
+window.addEventListener('online', getStatus);
+window.addEventListener('offline', getStatus);
+//getStatus({ type: 'ready' });
