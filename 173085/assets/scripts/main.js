65cc1187627e8365ce8335d242ba95cee8653aad
thirozumi <thirozumi@gmail.com>
2018-07-03 10:39:52 +0900

    added

--- 173085/assets/scripts/main.js
+++ 173085/assets/scripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
- let getStatus = function(e) {
-   let status = navigator.onLine ? 'online' : 'offline';
-   let b = document.getElementsByTagName('body')[0];
-   b.className = '';
-   b.classList.add(status);
- }
+let getStatus = function(e) {
+  let status = navigator.onLine ? 'online' : 'offline';
+  let b = document.getElementsByTagName('body')[0];
+  b.className = '';
+  b.classList.add(status);
+}
 
- window.addEventListener('online', getStatus);
- window.addEventListener('offline', getStatus);
- getStatus({ type: 'ready' });
+window.addEventListener('online', getStatus);
+window.addEventListener('offline', getStatus);
+
+getStatus({ type: 'ready' })
